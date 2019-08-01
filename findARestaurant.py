2596d788e5b9e036f7d5c08287b9026ca170a3cc
colm29 <colm29@gmail.com>
2019-08-01 23:40:47 +0100

    Add foursquare API section

---
+++ findARestaurant.py
@@ -0,0 +1,39 @@
+from geocode import getGeocodeLocation
+import json
+import httplib2
+
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+foursquare_client_id = "MTQOHSDG3131AEBRHX24W251S05WLMMQVTBX0KJCJEKY4EL4"
+foursquare_client_secret = "LUH0DVE4EOEMBKXSCGHZML3K1N2RMPDH101KAYPLHMEKM5UM"
+
+
+def findARestaurant(mealType,location):
+	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
+	coords = getGeocodeLocation(location)
+
+	
+	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
+	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+
+	url = 'https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s'% (foursquare_client_id, foursquare_client_secret, str(coords[0]),str(coords[1]),mealType)
+	h = httplib2.Http()
+	result = json.loads(h.request(url,'GET')[1])
+	resto = result['response']['venues'][0]['name]
+	#3. Grab the first restaurant
+	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+	#5. Grab the first image
+	#6. If no image is available, insert default a image url
+	#7. Return a dictionary containing the restaurant name, address, and image url	
+if __name__ == '__main__':
+	findARestaurant("Tacos", "Jakarta, Indonesia")
+	findARestaurant("Tapas", "Maputo, Mozambique")
+	findARestaurant("Falafel", "Cairo, Egypt")
+	findARestaurant("Spaghetti", "New Delhi, India")
+	findARestaurant("Cappuccino", "Geneva, Switzerland")
+	findARestaurant("Sushi", "Los Angeles, California")
+	findARestaurant("Steak", "La Paz, Bolivia")
+	findARestaurant("Gyros", "Sydney, Australia")
